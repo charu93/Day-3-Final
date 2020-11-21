4d0829916d4349f12a350d5675ac72a4ddda5388
charu93 <charmila0409@gmail.com>
2020-11-21 23:48:03 +0530

    Updated again

--- src/org/phone/ExternalStorage.java
+++ src/org/phone/ExternalStorage.java
@@ -4,4 +4,12 @@ public class ExternalStorage {
 public void size() {
 	System.out.println("Size");
 }
+public  void phonename() {
+	System.out.println("Iphone");
+}
+public static void main(String[] args) {
+	ExternalStorage e = new ExternalStorage();
+	e.size();
+	e.phonename();
+}
 }
